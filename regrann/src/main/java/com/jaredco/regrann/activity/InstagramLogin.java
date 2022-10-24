1aeb884e5534d1f83f4fffd7682a9624916a1b37
Steven Kader <stevenkader@gmail.com>
2022-10-24 16:57:04 -0400

    add files

--- regrann/src/main/java/com/jaredco/regrann/activity/InstagramLogin.java
+++ regrann/src/main/java/com/jaredco/regrann/activity/InstagramLogin.java
@@ -82,7 +82,7 @@ public class InstagramLogin extends AppCompatActivity {
         // remove wv from user agent
 
 
-        webview.getSettings().setUserAgentString("Mozilla/5.0 (iPhone; CPU iPhone OS 16_0_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1");
+    //    webview.getSettings().setUserAgentString("Mozilla/5.0 (iPhone; CPU iPhone OS 16_0_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1");
 
 
         webview.setWebViewClient(new WebViewClient() {
